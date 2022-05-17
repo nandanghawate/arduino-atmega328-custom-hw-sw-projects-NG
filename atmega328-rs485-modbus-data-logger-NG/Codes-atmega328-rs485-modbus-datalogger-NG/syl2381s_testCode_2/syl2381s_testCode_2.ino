a9dd819d03c6bfdbd9182f44c96a875a52b41ecf
Nandan <nandan.ghawate@gmail.com>
2022-05-17 23:39:29 +0530

    Update syl2381s_testCode_2.ino

--- atmega328-rs485-modbus-data-logger-NG/Codes-atmega328-rs485-modbus-datalogger-NG/syl2381s_testCode_2/syl2381s_testCode_2.ino
+++ atmega328-rs485-modbus-data-logger-NG/Codes-atmega328-rs485-modbus-datalogger-NG/syl2381s_testCode_2/syl2381s_testCode_2.ino
@@ -23,33 +23,39 @@ void setup() {
 
   Serial.begin(115200);
   PID_Serial.begin(PID_BAUD);
+ 
+  node.begin(1, PID_Serial);
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
   delay(1000);
-  Serial.print("Test CH's PID ");
+  Serial.println("Test CH's PID ");
   delay(1000);
+}
+void loop() {
+  Serial.println("1 register read :");
+  uint8_t readData;
+  readData = node.readHoldingRegisters(0x0164, 1);
+  if (readData == node.ku8MBSuccess) {
+    Serial.print("\nPV: ");
+    Serial.println(node.getResponseBuffer(0));
+  }
+  else {
+    Serial.print("\nError: ");
+    Serial.println(readData, HEX);
+  }
+  delay(5000);
+
+  Serial.println("2 register read :");
+  readData = node.readHoldingRegisters(0x0164, 2);
+
+  if (readData == node.ku8MBSuccess) {
+    Serial.print("\nPV: ");
+    Serial.println(node.getResponseBuffer(0));
+  }
+  else {
+    Serial.print("\nError: ");
+    Serial.println(readData, HEX);
+  }
+  delay(5000);
 }
 
-void loop() {
-  preTransmission();
-  Serial.print("Sending Command 1");
-  PID_Serial.write(0x05030164000285AC);
-  postTransmission();
-  int i = 0;
-  while (i<50000) {
-    if (PID_Serial.available()) {
-      int inByte = PID_Serial.read();
-      Serial.write(inByte);
-    }
-    i++;
-  }
-  Serial.print("Sending Command 2");
-  PID_Serial.print(0x05030164000285AC);
-  postTransmission();
-  i = 0;
-  while (i<50000) {
-    if (PID_Serial.available()) {
-      int inByte = PID_Serial.read();
-      Serial.write(inByte);
-    }
-    i++;
-  }
-}
